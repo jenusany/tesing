980d002446a2ff2cd2dd2143bc8e15f39550c989
jenusany <jenusany@outlook.com>
2024-07-17 14:50:06 -0400

    perms update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.getElementById('loginButton').onclick = function() {
     const appId = '803246971960239';
     const redirectUri = 'https://jenusany.github.io/tesing/';
-    const authUrl = `https://www.facebook.com/v11.0/dialog/oauth?client_id=${appId}&redirect_uri=${redirectUri}&scope=instagram_basic,pages_show_list`;
+    const authUrl = `https://www.facebook.com/v11.0/dialog/oauth?client_id=${appId}&redirect_uri=${redirectUri}&scope=instagram_basic,pages_show_list,business_management,pages_read_engagement`;
   
     window.location.href = authUrl;
   };
@@ -13,9 +13,7 @@ if (code) {
 fetch(`https://graph.facebook.com/v11.0/oauth/access_token?client_id=803246971960239&redirect_uri=https://jenusany.github.io/tesing/&client_secret=c19298b4ce75926bf2dc0177b77e5912&code=${code}`)
   .then(response => response.json())
   .then(data => {
-    console.log(data)
     const accessToken = data["access_token"];
-    console.log(accessToken)
 
     fetch(`https://graph.facebook.com/v20.0/me/businesses?access_token=${accessToken}`)
         .then(response => response.json())
